199d9cfd471af0eaa5b21f269e9f80e460a26aea
tadi05 <89161225+tadi05@users.noreply.github.com>
2021-09-12 20:25:50 +0100

    Add files via upload

---
+++ box.js
@@ -0,0 +1,18 @@
+class Box
+  {
+  constructor(){
+    this.x = 100;
+    this.y = 200;
+    this.w = 50;
+    this.h = 50;
+
+  }
+  show(){
+    rect(this.x, this.y, this.w, this.h);
+  }
+  set_speed(v){
+    this.x = this.x + v; 
+  }
+  }
+
+  
---
+++ sketch.js
@@ -0,0 +1,16 @@
+var box1;
+
+
+function setup() 
+{
+  createCanvas(400, 400);
+  box1 = new Box();
+}
+
+function draw() 
+{
+  background(220);
+  box1.show();
+  box1.set_speed(7);
+}
+
